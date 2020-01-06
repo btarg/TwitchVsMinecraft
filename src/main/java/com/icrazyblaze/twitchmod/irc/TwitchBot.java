6aaa1dcce4251454536bdc9d1cefc56ab22db41f
iCrazyBlaze <iCrazyBlaze@users.noreply.github.com>
2020-01-06 18:13:58 +0000

    Show chat roles on hover

--- src/main/java/com/icrazyblaze/twitchmod/irc/TwitchBot.java
+++ src/main/java/com/icrazyblaze/twitchmod/irc/TwitchBot.java
@@ -6,6 +6,7 @@ import com.icrazyblaze.twitchmod.BotCommands;
 import com.icrazyblaze.twitchmod.chat.ChatPicker;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.event.HoverEvent;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.ConnectEvent;
 import org.pircbotx.hooks.events.DisconnectEvent;
@@ -33,6 +34,9 @@ public class TwitchBot extends ListenerAdapter {
 
         TextFormatting format = TextFormatting.WHITE;
 
+        TextComponentString showText;
+        String role = null;
+
         if (BotConfig.showChatMessages) {
 
             if (tags != null) {
@@ -40,16 +44,25 @@ public class TwitchBot extends ListenerAdapter {
                 if (tags.get("badges").contains("broadcaster/1")) {
                     format = TextFormatting.GOLD;
                     forceCommands = true; // Force commands to execute instantly for broadcaster testing
+                    role = "Broadcaster";
                 } else if (tags.get("badges").contains("subscriber/1")) {
                     format = TextFormatting.AQUA;
+                    role = "Subscriber";
                 } else if (tags.get("badges").contains("moderator/1")) {
                     format = TextFormatting.GREEN;
+                    role = "Moderator";
                 }
             }
 
             if (!message.startsWith(BotConfig.prefix) || BotConfig.showCommands) {
 
-                BotCommands.player().sendMessage(new TextComponentString(TextFormatting.WHITE + "<" + TextFormatting.DARK_PURPLE + "Twitch " + format + sender + TextFormatting.WHITE + "> " + message));
+                showText = new TextComponentString(TextFormatting.WHITE + "<" + TextFormatting.DARK_PURPLE + "Twitch " + format + sender + TextFormatting.WHITE + "> " + message);
+
+                if (role != null) {
+                    showText.getStyle().setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponentString(format + role)));
+                }
+
+                BotCommands.player().sendMessage(showText);
 
             }
 
